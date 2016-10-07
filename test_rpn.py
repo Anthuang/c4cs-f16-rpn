984478c8d6aa3acdc806dbde8765a95e1f3f5c35
Anthony Huang <anthuang@umich.edu>
2016-10-07 11:42:36 -0700

    tests * /

--- test_rpn.py
+++ test_rpn.py
@@ -9,6 +9,12 @@ class TestBasics(unittest.TestCase):
   def test_subtract(self):
     result = rpn.calculate("5 3 -")
     self.assertEqual(2, result)
+  def test_multiply(self):
+    result = rpn.calculate("3 3 *")
+    self.assertEqual(9, result)
+  def test_divide(self):
+    result = rpn.calculate("8 2 /")
+    self.assertEqual(4, result)
   def test_toomanythings(self):
     with self.assertRaises(TypeError):
       rpn.calculate("1 2 3 +")
